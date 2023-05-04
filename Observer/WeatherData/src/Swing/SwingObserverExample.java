ec7511cf94d8ede3cbfc5911399288b1c0d9900d
thiago-yuiti <61800646+thiago-yuiti@users.noreply.github.com>
2023-05-03 23:53:54 -0300

    swing

---
+++ Observer/WeatherData/src/Swing/SwingObserverExample.java
@@ -0,0 +1,35 @@
+package Swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        JButton button = new JButton("Should i do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+    }
+}
+
+class AngelListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        System.out.println("Don't do it, you might regret it!");
+    }
+}
+
+class DevilListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        System.out.println("Come on, do it!");
+    }
+}
--- Observer/WeatherData/src/WeatherData/WeatherStation.java
+++ Observer/WeatherData/src/WeatherData/WeatherStation.java
@@ -7,7 +7,7 @@ public class WeatherStation {
 //        StatisticsDisplay  statisticsDisplay = new Statistics
         HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay();
 
-        heatIndexDisplay.compute
+
 
     weatherData.setMeasurements(80,65,30.4f);
     weatherData.setMeasurements(82, 70, 29.2f);
