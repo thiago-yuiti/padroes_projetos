5ce2f972f6c6b9b20614e40526ad3f9a9d8f59cc
thiago-yuiti <thiago.tomaru@bandtec.com.br>
2024-01-22 00:16:44 -0300

    command

---
+++ Singleton/Command/src/GarageDoorOpenCommand.java
@@ -0,0 +1,15 @@
+public class GarageDoorOpenCommand implements Command {
+    public void up() {
+        System.out.println("");
+    }
+
+    public void down() {}
+
+    public void stop() {}
+
+    public void lightOn() {}
+
+    public void lightOff() {
+
+    }
+}
